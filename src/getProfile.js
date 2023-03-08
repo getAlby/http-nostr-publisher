74cc2940012304a823e3ccffd0096f3bb052ee4f
Michael Bumann <hello@michaelbumann.com>
2023-03-08 09:08:47 +0100

    HTTP 202 if no profile is available and the profile is still fetched

--- src/getProfile.js
+++ src/getProfile.js
@@ -13,23 +13,24 @@ export const getProfile = async (request, env, ctx) => {
 
   const setCache = (key, data) => env.PROFILES.put(key, data, {
     metadata: { createdAt: Date.now() },
-    // key expires after 2 days
-    expirationTtl: 2 * 24 * HOUR
+    // key expires after 7 days
+    expirationTtl: 7 * 24 * HOUR
   })
   const getCache = key => env.PROFILES.getWithMetadata(key)
 
   const fetchAndCache = async (pubkey) => {
-    const result = await Promise.any(fetchProfiles(pubkey,relayUrls));
+    const result = await Promise.any(fetchProfiles(pubkey, relayUrls));
     return await setCache(pubkey, JSON.stringify(result));
   }
-  
+
   const { value, metadata } = await getCache(pubkey);
-  
+
   if (!metadata || metadata.createdAt + 3 * HOUR * 1000 > Date.now()) {
     ctx.waitUntil(fetchAndCache(pubkey, relayUrls));
   }
 
-  return new Response(value, { 
+  return new Response(value, {
+    status: (value ? 200 : 202),
     headers: {
       'content-type': 'application/json;charset=UTF-8',
     }
